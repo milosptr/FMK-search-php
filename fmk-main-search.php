f84625810f5f29387dae07e9c28d19d962e52fb1
Miloš Petrović <milosptr@hotmail.com>
2018-02-21 21:36:12 +0100

    Update fmk-main-search.php

--- fmk-main-search.php
+++ fmk-main-search.php
@@ -1,5 +1,5 @@
 <?php
-	 $db = new mysqli('localhost', 'alphaeff_fmk', 'J^9}qKeKTel4', 'alphaeff_fmkstage');
+	 $db = new mysqli('localhost', '', '', '');
  ?>
 
  <form class="form-inline"  method="POST" action="" accept-charset="utf-8">
